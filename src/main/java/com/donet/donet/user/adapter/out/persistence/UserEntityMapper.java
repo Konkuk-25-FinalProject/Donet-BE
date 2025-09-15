3161c9d66af69df16ad31c254c77df04ac8add31
JangIkhwan <bluensky0213@gmail.com>
2025-09-15 16:22:28 +0900

    [Feat] #3 유저 JPA 엔티티 작성

---
+++ src/main/java/com/donet/donet/user/adapter/out/persistence/UserEntityMapper.java
@@ -0,0 +1,15 @@
+package com.donet.donet.user.adapter.out.persistence;
+
+import com.donet.donet.user.domain.User;
+import org.springframework.stereotype.Component;
+
+@Component
+public class UserEntityMapper {
+    public User mapToDomainEntity(UserJpaEntity jpaEntity) {
+        return new User(jpaEntity.getId(),
+                jpaEntity.getNickname(),
+                jpaEntity.getProfileImage(),
+                jpaEntity.getLoginProvider(),
+                jpaEntity.getLoginId());
+    }
+}
