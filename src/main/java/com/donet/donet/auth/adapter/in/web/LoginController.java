2ec3e69ef017274e2081d77cda54bf5c021f8d95
JangIkhwan <bluensky0213@gmail.com>
2025-09-17 19:41:00 +0900

    [Fix] #3 LoginController에 파라미터 관련 어노테이션 추가

--- src/main/java/com/donet/donet/auth/adapter/in/web/LoginController.java
+++ src/main/java/com/donet/donet/auth/adapter/in/web/LoginController.java
@@ -4,8 +4,10 @@ import com.donet.donet.auth.application.port.in.LoginCommand;
 import com.donet.donet.auth.application.port.in.LoginResponse;
 import com.donet.donet.auth.application.port.in.LoginUsecase;
 import com.donet.donet.global.response.BaseResponse;
+import jakarta.validation.constraints.NotBlank;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
@@ -14,7 +16,7 @@ public class LoginController {
     private final LoginUsecase loginUsecase;
 
     @PostMapping("/auth/login/kakao")
-    public BaseResponse<LoginResponse> kakaoLogin(String code){
+    public BaseResponse<LoginResponse> kakaoLogin(@RequestParam("code") @NotBlank String code){
         return new BaseResponse<>(loginUsecase.login(new LoginCommand(code)));
     }
 }
