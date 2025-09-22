c5b2d9d793b6fdd747e09ec97945874f1ab82f73
JangIkhwan <bluensky0213@gmail.com>
2025-09-22 20:58:39 +0900

    [Fix] #7 토큰 재발급 유스케이스 수정

--- src/main/java/com/donet/donet/auth/application/ReissueTokenService.java
+++ src/main/java/com/donet/donet/auth/application/ReissueTokenService.java
@@ -5,9 +5,14 @@ import com.donet.donet.auth.application.port.in.dto.ReissueTokenCommand;
 import com.donet.donet.auth.application.port.in.dto.ReissueTokenResponse;
 import com.donet.donet.auth.application.port.out.CacheRefreshTokenPort;
 import com.donet.donet.auth.application.port.out.TokenIssuerPort;
+import com.donet.donet.global.exception.CustomException;
+import com.donet.donet.user.application.port.out.FindUserPort;
+import com.donet.donet.user.domain.User;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+
+import static com.donet.donet.global.response.status.BaseExceptionResponseStatus.*;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -15,28 +20,33 @@ import org.springframework.stereotype.Service;
 public class ReissueTokenService implements ReissueTokenUsecase {
     private final CacheRefreshTokenPort cacheRefreshTokenPort;
     private final TokenIssuerPort tokenIssuerPort;
+    private final FindUserPort findUserPort;
 
     @Override
     public ReissueTokenResponse reissue(ReissueTokenCommand command) {
         // 토큰 검증
         if(!tokenIssuerPort.validate(command.refreshToken())){
-            throw new RuntimeException();
+            throw new CustomException(INVALID_JWT);
         }
+
+        // 유저 조회
+        Long userId = tokenIssuerPort.resolveUserId(command.refreshToken());
+        User user = findUserPort.findById(userId)
+                .orElseThrow(() -> new CustomException(USER_NOT_FOUND));
 
         // 레디스의 토큰과 일치하는지 확인
-       String oldToken = cacheRefreshTokenPort.find(command.userId());
-        
+        String oldToken = cacheRefreshTokenPort.findById(user.getId())
+                .orElseThrow(() -> new CustomException(REFRESH_TOKEN_NOT_FOUND));
         if(!oldToken.equals(command.refreshToken())){
-           throw new RuntimeException();
+            throw new CustomException(REFRESH_TOKEN_NOT_FOUND);
         }
-        // 토큰 재발급
-        String accessToken = tokenIssuerPort.createAccessToken(command.userId());
-        String refreshToken = tokenIssuerPort.createRefreshToken(command.userId());
 
-        // 토큰을 레디스에 저장
-        cacheRefreshTokenPort.update(command.userId(), refreshToken);
+        String accessToken = tokenIssuerPort.createAccessToken(user.getId());
+        String refreshToken = tokenIssuerPort.createRefreshToken(user.getId());
 
-        // 반환
+        // 새 토큰을 레디스에 저장
+        cacheRefreshTokenPort.update(user.getId(), refreshToken);
+
         return new ReissueTokenResponse(accessToken, refreshToken);
     }
 }
--- src/main/java/com/donet/donet/auth/application/port/in/dto/ReissueTokenCommand.java
+++ src/main/java/com/donet/donet/auth/application/port/in/dto/ReissueTokenCommand.java
@@ -1,7 +1,6 @@
 package com.donet.donet.auth.application.port.in.dto;
 
 public record ReissueTokenCommand(
-        Long userId,
         String refreshToken
 ) {
 }
