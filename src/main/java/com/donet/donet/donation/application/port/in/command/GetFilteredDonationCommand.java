0c4fc82e6307a29ee8334cdcd4fbf0d29e2415ae
imscow11253 <seaweed8759@naver.com>
2025-09-22 17:56:54 +0900

    [Update] #8 usecase command에 userId 필드값 추가

--- src/main/java/com/donet/donet/donation/application/port/in/command/GetFilteredDonationCommand.java
+++ src/main/java/com/donet/donet/donation/application/port/in/command/GetFilteredDonationCommand.java
@@ -6,6 +6,7 @@ import org.springframework.data.domain.Pageable;
 import java.util.List;
 
 public record GetFilteredDonationCommand(
+        Long userId,
         List<Category> categories,
         Pageable pageable
 ) {
