eef77b3a027a95b5a77786cda27c13941468ef30
JangIkhwan <bluensky0213@gmail.com>
2025-09-24 15:47:35 +0900

    [Feat] #12 유저 엔티티에 필드 추가 및 유저 프로필 수정 메서드 추가

--- src/main/java/com/donet/donet/user/domain/User.java
+++ src/main/java/com/donet/donet/user/domain/User.java
@@ -1,7 +1,10 @@
 package com.donet.donet.user.domain;
 
+import com.donet.donet.global.exception.CustomException;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+
+import static com.donet.donet.global.response.status.BaseExceptionResponseStatus.USER_DOMAIN_RULE_VIOLATION;
 
 @AllArgsConstructor
 @Getter
@@ -11,4 +14,20 @@ public class User {
     private String profileImage;
     private String loginProvider;
     private String loginId;
+    private String walletAddress;
+
+    public void editProfileImage(String profileImage) {
+        if(profileImage == null){
+            throw new CustomException(USER_DOMAIN_RULE_VIOLATION);
+        }
+        this.profileImage = profileImage;
+    }
+
+    public void editProfileData(String nickname, String walletAddress) {
+        if(nickname == null || nickname.isBlank() || walletAddress == null || walletAddress.isBlank()){
+            throw new CustomException(USER_DOMAIN_RULE_VIOLATION);
+        }
+        this.nickname = nickname;
+        this.walletAddress = walletAddress;
+    }
 }
