2a2cebda807021610f7e021e7c8908a603178553
JangIkhwan <bluensky0213@gmail.com>
2025-09-24 13:25:12 +0900

    [Fix] #7 포트의 출력 모델에서 불필요한 필드 제거

--- src/main/java/com/donet/donet/auth/application/port/out/KakaoOAuthToken.java
+++ src/main/java/com/donet/donet/auth/application/port/out/KakaoOAuthToken.java
@@ -1,11 +1,6 @@
 package com.donet.donet.auth.application.port.out;
 
 public record KakaoOAuthToken(
-        String access_token,
-        String token_type,
-        String refresh_token,
-        int expires_in,
-        String scope,
-        int refresh_token_expires_in
+        String access_token
 ) {
 }
