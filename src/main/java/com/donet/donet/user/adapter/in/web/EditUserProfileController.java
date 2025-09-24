63cafb54dc86ec0420f51505895f8039397e6842
JangIkhwan <bluensky0213@gmail.com>
2025-09-24 15:49:18 +0900

    [Feat] #12 유저 프로필 수정 API 웹 어댑터 구현

---
+++ src/main/java/com/donet/donet/user/adapter/in/web/EditUserProfileController.java
@@ -0,0 +1,26 @@
+package com.donet.donet.user.adapter.in.web;
+
+import com.donet.donet.global.annotation.CurrentUserId;
+import com.donet.donet.global.response.BaseResponse;
+import com.donet.donet.user.application.port.in.EditUserProfileUsecase;
+import com.donet.donet.user.adapter.in.web.dto.EditUserProfileRequest;
+import com.donet.donet.user.application.port.dto.EditUserProfileCommand;
+import lombok.RequiredArgsConstructor;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+@RequiredArgsConstructor
+@RestController
+public class EditUserProfileController {
+    private final EditUserProfileUsecase editUserProfileUsecase;
+    @PostMapping("/users/me")
+    public BaseResponse<Void> getUserProfile(@CurrentUserId Long userId,
+                                                                @RequestPart MultipartFile profileImage,
+                                                                @Validated @RequestPart EditUserProfileRequest request){
+        editUserProfileUsecase.editProfile(new EditUserProfileCommand(userId, profileImage, request.nickname(), request.walletAddress()));
+        return new BaseResponse<>(null);
+    }
+}
