9bbfcd0570f4f84782fc6b814a9ae236a9468bbf
JangIkhwan <bluensky0213@gmail.com>
2025-09-24 15:30:00 +0900

    [Feat] #12 유저 정보 수정 유스케이스 구현

---
+++ src/main/java/com/donet/donet/user/application/port/in/EditUserProfileUsecase.java
@@ -0,0 +1,7 @@
+package com.donet.donet.user.application.port.in;
+
+import com.donet.donet.user.application.port.dto.EditUserProfileCommand;
+
+public interface EditUserProfileUsecase {
+    void editProfile(EditUserProfileCommand command);
+}
