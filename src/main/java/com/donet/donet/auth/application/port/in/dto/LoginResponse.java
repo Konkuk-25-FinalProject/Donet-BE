6b381974a8c40954f8c0a37349f55c23c7e6192c
JangIkhwan <bluensky0213@gmail.com>
2025-09-27 16:07:45 +0900

    [Doc] #7 카카오 로그인 API 명세

--- src/main/java/com/donet/donet/auth/application/port/in/dto/LoginResponse.java
+++ src/main/java/com/donet/donet/auth/application/port/in/dto/LoginResponse.java
@@ -1,7 +1,12 @@
 package com.donet.donet.auth.application.port.in.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
+@Schema(description = "로그인 응답")
 public record LoginResponse(
+        @Schema(description = "엑세스 토큰")
         String accessToken,
+        @Schema(description = "리프레시 토큰")
         String refreshToken
 ) {
 }
