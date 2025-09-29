02acbbeb686935cf36fea53da2c25d7d7572b13c
JangIkhwan <bluensky0213@gmail.com>
2025-09-29 12:55:19 +0900

    [Feat] #12 유스케이스에 유저 프로필 항목별 수정 메서드 추가

--- src/main/java/com/donet/donet/user/application/port/in/EditUserProfileUsecase.java
+++ src/main/java/com/donet/donet/user/application/port/in/EditUserProfileUsecase.java
@@ -1,7 +1,15 @@
 package com.donet.donet.user.application.port.in;
 
 import com.donet.donet.user.application.port.in.dto.EditUserProfileCommand;
+import org.springframework.web.multipart.MultipartFile;
 
 public interface EditUserProfileUsecase {
     void editProfile(EditUserProfileCommand command);
+
+    void editProfileImage(Long userId, MultipartFile profileImage);
+
+    void editNickname(Long userId, String nickname);
+
+    void editWalletAddress(Long userId, String walletAddress);
+
 }
