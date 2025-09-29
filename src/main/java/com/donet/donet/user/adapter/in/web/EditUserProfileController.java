727becfab49698b33190abcab383dd4fc1f6f407
JangIkhwan <bluensky0213@gmail.com>
2025-09-29 13:00:34 +0900

    [Feat] #12 유저 프로필 항목별 수정 메서드 명세 추가

--- src/main/java/com/donet/donet/user/adapter/in/web/EditUserProfileController.java
+++ src/main/java/com/donet/donet/user/adapter/in/web/EditUserProfileController.java
@@ -2,19 +2,21 @@ package com.donet.donet.user.adapter.in.web;
 
 import com.donet.donet.global.annotation.CurrentUserId;
 import com.donet.donet.global.response.BaseResponse;
+import com.donet.donet.global.swagger.CustomExceptionDescription;
 import com.donet.donet.user.application.port.in.EditUserProfileUsecase;
 import com.donet.donet.user.application.port.in.dto.EditUserProfileCommand;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import jakarta.validation.constraints.NotBlank;
 import lombok.RequiredArgsConstructor;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import static com.donet.donet.global.swagger.SwaggerResponseDescription.DEFAULT;
+import static com.donet.donet.global.swagger.SwaggerResponseDescription.EDIT_USER_PROFILE;
 
 @RestController
 @RequiredArgsConstructor
@@ -39,25 +41,46 @@ public class EditUserProfileController implements UserController{
         return new BaseResponse<>(null);
     }
 
-    @PatchMapping(value = "/users/me/profile-image")
-    public BaseResponse<Void> editUserProfileImage(@CurrentUserId Long userId,
-                                                   @RequestPart MultipartFile profileImage
+    @Operation(
+            summary = "유저 프로필 이미지 수정 API",
+            description = """
+                    유저의 프로필 이미지를 수정할 수 있다. 
+                    """
+    )
+    @CustomExceptionDescription(EDIT_USER_PROFILE)
+    @PatchMapping(value = "/users/me/profile-image", consumes = {"multipart/form-data"})
+    public BaseResponse<Void> editUserProfileImage(@Parameter(hidden = true) @CurrentUserId Long userId,
+                                                   @Parameter(name = "변경할 프로필 이미지") @RequestPart MultipartFile profileImage
     ){
         editUserProfileUsecase.editProfileImage(userId, profileImage);
         return new BaseResponse<>(null);
     }
 
+    @Operation(
+            summary = "유저 닉네임 수정 API",
+            description = """
+                    유저의 닉네임을 수정할 수 있다. 
+                    """
+    )
+    @CustomExceptionDescription(DEFAULT)
     @PatchMapping(value = "/users/me/nickname")
-    public BaseResponse<Void> editUserNickname(@CurrentUserId Long userId,
-                                               @Validated @NotBlank String nickname
+    public BaseResponse<Void> editUserNickname(@Parameter(hidden = true) @CurrentUserId Long userId,
+                                               @Parameter(name = "변경할 닉네임", required = true) @NotBlank String nickname
     ){
         editUserProfileUsecase.editNickname(userId, nickname);
         return new BaseResponse<>(null);
     }
 
+    @Operation(
+            summary = "유저 지갑주소 수정 API",
+            description = """
+                    유저의 지갑주소를 수정할 수 있다.
+                    """
+    )
+    @CustomExceptionDescription(DEFAULT)
     @PatchMapping(value = "/users/me/wallet-address")
-    public BaseResponse<Void> editUserWalletAddress(@CurrentUserId Long userId,
-                                                    @Validated @NotBlank String walletAddress
+    public BaseResponse<Void> editUserWalletAddress(@Parameter(hidden = true) @CurrentUserId Long userId,
+                                                    @Parameter(name= "변경할 지갑주소", required = true) @NotBlank String walletAddress
     ){
         editUserProfileUsecase.editWalletAddress(userId, walletAddress);
         return new BaseResponse<>(null);
