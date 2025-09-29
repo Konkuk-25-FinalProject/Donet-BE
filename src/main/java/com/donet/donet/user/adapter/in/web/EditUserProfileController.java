583907afb89b128b931f0a56c7929c1a756e7a36
JangIkhwan <bluensky0213@gmail.com>
2025-09-29 12:56:24 +0900

    [Feat] #12 컨트롤러 유저 프로필 항목별 수정 메서드 구현

--- src/main/java/com/donet/donet/user/adapter/in/web/EditUserProfileController.java
+++ src/main/java/com/donet/donet/user/adapter/in/web/EditUserProfileController.java
@@ -8,10 +8,13 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import jakarta.validation.constraints.NotBlank;
 import lombok.RequiredArgsConstructor;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
+
 
 @RestController
 @RequiredArgsConstructor
@@ -19,20 +22,44 @@ public class EditUserProfileController implements UserController{
     private final EditUserProfileUsecase editUserProfileUsecase;
 
     @Operation(
-            summary = "유저 프로필 수정 API",
+            summary = "온보딩 유저 프로필 등록 API",
             description = """
-                    유저의 프로필 이미지, 닉네임, 지갑주소를 수정할 수 있다. 
+                    유저의 프로필 이미지, 닉네임, 지갑주소를 등록할 수 있다. 
                     프로필 이미지를 변경하지 않으려면 요청에 포함하지 않아야 한다. 
                     닉네임과 지갑주소는 필수 값이라서 기존 값을 그대로 보내야 한다
                     """
     )
-    @PatchMapping(value = "/users/me", consumes = {"multipart/form-data"})
+    @PostMapping(value = "/users/me", consumes = {"multipart/form-data"})
     public BaseResponse<Void> editUserProfile(@Parameter(hidden = true) @CurrentUserId Long userId,
                                               @Parameter(name = "변경할 프로필 이미지", required = false) @RequestPart(required = false) MultipartFile profileImage,
                                               @Parameter(name = "변경하거나 유지할 닉네임", required = true) @NotBlank String nickname,
                                               @Parameter(name= "변경하거나 유지할 지갑주소", required = true) @NotBlank String walletAddress
-                                              ){
+    ){
         editUserProfileUsecase.editProfile(new EditUserProfileCommand(userId, profileImage, nickname, walletAddress));
+        return new BaseResponse<>(null);
+    }
+
+    @PatchMapping(value = "/users/me/profile-image")
+    public BaseResponse<Void> editUserProfileImage(@CurrentUserId Long userId,
+                                                   @RequestPart MultipartFile profileImage
+    ){
+        editUserProfileUsecase.editProfileImage(userId, profileImage);
+        return new BaseResponse<>(null);
+    }
+
+    @PatchMapping(value = "/users/me/nickname")
+    public BaseResponse<Void> editUserNickname(@CurrentUserId Long userId,
+                                               @Validated @NotBlank String nickname
+    ){
+        editUserProfileUsecase.editNickname(userId, nickname);
+        return new BaseResponse<>(null);
+    }
+
+    @PatchMapping(value = "/users/me/wallet-address")
+    public BaseResponse<Void> editUserWalletAddress(@CurrentUserId Long userId,
+                                                    @Validated @NotBlank String walletAddress
+    ){
+        editUserProfileUsecase.editWalletAddress(userId, walletAddress);
         return new BaseResponse<>(null);
     }
 }
