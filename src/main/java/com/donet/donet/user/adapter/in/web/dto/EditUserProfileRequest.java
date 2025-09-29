24e8113a50e4b23b39e47d58460d737ec3e87fc9
JangIkhwan <bluensky0213@gmail.com>
2025-09-29 11:36:42 +0900

    [Doc] #12 유저 프로필 수정 API 명세

--- src/main/java/com/donet/donet/user/adapter/in/web/dto/EditUserProfileRequest.java
+++ src/main/java/com/donet/donet/user/adapter/in/web/dto/EditUserProfileRequest.java
@@ -1,9 +1,14 @@
 package com.donet.donet.user.adapter.in.web.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 
+@Schema(description = "프로필 수정 요청")
 public record EditUserProfileRequest(
+    @Schema(description = "변경하거나 유지할 닉네임")
     @NotBlank String nickname,
+
+    @Schema(description = "변경하거나 유지할 지갑주소")
     @NotBlank String walletAddress
 ) {
 }
