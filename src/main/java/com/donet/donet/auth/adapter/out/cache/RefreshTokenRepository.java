0d23f081361eddb0ef1f0f8fbffc4decc30e4ff4
JangIkhwan <bluensky0213@gmail.com>
2025-09-29 16:57:41 +0900

    [Feat] #14 어댑터에 리프레시 토큰 삭제 구현 및 리포지토리 수정

--- src/main/java/com/donet/donet/auth/adapter/out/cache/RefreshTokenRepository.java
+++ src/main/java/com/donet/donet/auth/adapter/out/cache/RefreshTokenRepository.java
@@ -2,8 +2,4 @@ package com.donet.donet.auth.adapter.out.cache;
 
 import org.springframework.data.repository.CrudRepository;
 
-import java.util.Optional;
-
-public interface RefreshTokenRepository extends CrudRepository<RefreshToken, String> {
-    Optional<RefreshToken> findByUserId(Long userId);
-}
+public interface RefreshTokenRepository extends CrudRepository<RefreshToken, Long> {}
