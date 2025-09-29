f292c10359392a9b3da8fa9535a9f6686cff4f3c
JangIkhwan <bluensky0213@gmail.com>
2025-09-29 10:49:24 +0900

    [Fix] #12 유저 프로필 수정 API의 http메서드와 컨트롤러 메서드명 수정

--- src/main/java/com/donet/donet/user/adapter/in/web/EditUserProfileController.java
+++ src/main/java/com/donet/donet/user/adapter/in/web/EditUserProfileController.java
@@ -7,17 +7,18 @@ import com.donet.donet.user.adapter.in.web.dto.EditUserProfileRequest;
 import com.donet.donet.user.application.port.in.dto.EditUserProfileCommand;
 import lombok.RequiredArgsConstructor;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 @RequiredArgsConstructor
 @RestController
-public class EditUserProfileController {
+public class EditUserProfileController implements UserController{
     private final EditUserProfileUsecase editUserProfileUsecase;
-    @PostMapping("/users/me")
-    public BaseResponse<Void> getUserProfile(@CurrentUserId Long userId,
+
+    @PatchMapping("/users/me")
+    public BaseResponse<Void> editUserProfile(@CurrentUserId Long userId,
                                                                 @RequestPart(required = false) MultipartFile profileImage,
                                                                 @Validated @RequestPart EditUserProfileRequest request){
         editUserProfileUsecase.editProfile(new EditUserProfileCommand(userId, profileImage, request.nickname(), request.walletAddress()));
