2079abc2d7635535a4d08a240af610d10f921145
imscow11253 <seaweed8759@naver.com>
2025-10-01 03:32:46 +0900

    [Refactor] #9 Category enum 클래스를 도메인 일반 클래스 리팩토링

--- src/main/java/com/donet/donet/donation/adapter/out/persistence/category/GetInterestedCategoriesAdapter.java
+++ src/main/java/com/donet/donet/donation/adapter/out/persistence/category/GetInterestedCategoriesAdapter.java
@@ -1,15 +1,19 @@
-package com.donet.donet.donation.adapter.out.jpa;
+package com.donet.donet.donation.adapter.out.persistence;
 
 import com.donet.donet.donation.application.port.out.GetInterestedCategoriesPort;
 import com.donet.donet.global.enums.Category;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
 
 @Component
+@RequiredArgsConstructor
 public class GetInterestedCategoriesAdapter implements GetInterestedCategoriesPort {
+    private final
+
     @Override
-    public List<Category> getInterestedCategories(Long userId) {
+    public List<Category> findInterestedCategories(Long userId) {
         return List.of();
     }
 }
--- src/main/java/com/donet/donet/donation/adapter/out/persistence/donation/DonationPersistenceAdapter.java
+++ src/main/java/com/donet/donet/donation/adapter/out/persistence/donation/DonationPersistenceAdapter.java
@@ -1,13 +1,20 @@
-package com.donet.donet.donation.adapter.out.jpa;
+package com.donet.donet.donation.adapter.out.persistence;
 
 import com.donet.donet.donation.application.port.out.FindDonationPort;
 import com.donet.donet.donation.domain.Donation;
 import com.donet.donet.global.enums.Category;
+import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Component;
 
 import java.util.List;
 
+@Component
+@RequiredArgsConstructor
 public class DonationPersistenceAdapter implements FindDonationPort {
+    private final DonationRepository donationRepository;
+    private final DonationMapper donationMapper;
+
     @Override
     public List<Donation> findFilterDonationPage(List<Category> categories, Pageable pageable) {
         return List.of();
