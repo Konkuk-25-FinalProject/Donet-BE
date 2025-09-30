244b1472616069f9be127722ac5e623569a7fead
imscow11253 <seaweed8759@naver.com>
2025-10-01 05:05:55 +0900

    [Feat] #10 카테고리 도메인 엔티티에 정적 팩토리 메소드 추가

--- src/main/java/com/donet/donet/donation/domain/Category.java
+++ src/main/java/com/donet/donet/donation/domain/Category.java
@@ -8,4 +8,18 @@ import lombok.Getter;
 public class Category {
     private Long id;
     private String name;
+
+    public static Category of(String name) {
+        return new Category(getIdFromName(name), name);
+    }
+
+    private static Long getIdFromName(String name) {
+        if(name.equals("EDUCATION")) return 1L;
+        if(name.equals("HOBBY")) return 2L;
+        if(name.equals("MEDICAL")) return 3L;
+        if(name.equals("FOOD")) return 4L;
+        if(name.equals("ETC")) return 5L;
+        return 0L;
+        //TODO: exception 발생
+    }
 }
