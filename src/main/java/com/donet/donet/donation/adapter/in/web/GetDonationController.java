4b6705a620f38135de8396915bed6d410d9ec32a
imscow11253 <seaweed8759@naver.com>
2025-10-19 16:50:53 +0900

    [Feat] #21 기부 상세 조회 Web Adapter 추가

---
+++ src/main/java/com/donet/donet/donation/adapter/in/web/GetDonationController.java
@@ -0,0 +1,21 @@
+package com.donet.donet.donation.adapter.in.web;
+
+import com.donet.donet.donation.application.port.in.GetDonationDetailUsecase;
+import com.donet.donet.donation.application.port.in.response.GetDonationDetailResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/donation")
+public class GetDonationController {
+    private final GetDonationDetailUsecase getDonationDetailUsecase;
+
+    @GetMapping("/{donationId}/detail")
+    public GetDonationDetailResponse getDonationDetail(@PathVariable long donationId) {
+        return getDonationDetailUsecase.getDonationDetail(donationId);
+    }
+}
