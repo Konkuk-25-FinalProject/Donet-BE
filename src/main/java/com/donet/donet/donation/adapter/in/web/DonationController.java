43b482e4c6d93e020a8e8b0c6e78d1ea90d84e9a
imscow11253 <seaweed8759@naver.com>
2025-10-19 16:52:21 +0900

    [Feat] #21 Donation 관련 Swagger Tag 어노테이션을 위한 인터페이스 컨트롤러 생성

---
+++ src/main/java/com/donet/donet/donation/adapter/in/web/DonationController.java
@@ -0,0 +1,10 @@
+package com.donet.donet.donation.adapter.in.web;
+
+import io.swagger.v3.oas.annotations.tags.Tag;
+
+@Tag(
+        name = "Donation",
+        description = "기부 관련 API들"
+)
+public interface DonationController {
+}
