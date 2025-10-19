9a575fabbceae739e29f930b8d99986d316d8eb1
imscow11253 <seaweed8759@naver.com>
2025-10-19 15:20:30 +0900

    [Refactor] #19 기부 상세 조회 유스케이스에 인자 타입 수정

--- src/main/java/com/donet/donet/donation/application/port/in/GetDonationDetailUsecase.java
+++ src/main/java/com/donet/donet/donation/application/port/in/GetDonationDetailUsecase.java
@@ -3,5 +3,5 @@ package com.donet.donet.donation.application.port.in;
 import com.donet.donet.donation.application.port.in.response.GetDonationDetailResponse;
 
 public interface GetDonationDetailUsecase {
-    GetDonationDetailResponse getDonationDetail(String donationId);
+    GetDonationDetailResponse getDonationDetail(Long donationId);
 }
