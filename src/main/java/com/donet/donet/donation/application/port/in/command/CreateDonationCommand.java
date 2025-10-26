9168fe5e52c041a65e7e2380f6675e1099ce7c45
imscow11253 <seaweed8759@naver.com>
2025-10-26 14:42:07 +0900

    [Feat] #24 기부 생성을 위한 application layer command record 클래스 정의

---
+++ src/main/java/com/donet/donet/donation/application/port/in/command/CreateDonationCommand.java
@@ -0,0 +1,9 @@
+package com.donet.donet.donation.application.port.in.command;
+
+public record CreateDonationCommand (
+        Long userId,
+        Long donationId,
+        Long donationAmount,
+        boolean isAnonymous
+){
+}
