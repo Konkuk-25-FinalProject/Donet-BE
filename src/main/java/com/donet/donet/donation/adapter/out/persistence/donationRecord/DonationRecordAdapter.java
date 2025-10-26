18f6df09b22a790939f075a0a9bc6756d00dbfef
imscow11253 <seaweed8759@naver.com>
2025-10-26 16:14:06 +0900

    [Feat] #25 기부 기록 생성 out port interface 구현체 로직 구현하기

--- src/main/java/com/donet/donet/donation/adapter/out/persistence/donationRecord/DonationRecordAdapter.java
+++ src/main/java/com/donet/donet/donation/adapter/out/persistence/donationRecord/DonationRecordAdapter.java
@@ -2,12 +2,23 @@ package com.donet.donet.donation.adapter.out.persistence.donationRecord;
 
 import com.donet.donet.donation.application.port.out.CreateDonationRecordPort;
 import com.donet.donet.donation.domain.DonationRecord;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor
 public class DonationRecordAdapter implements CreateDonationRecordPort {
+    private final DonationRecordRepository donationRecordRepository;
+    private final DonationRecordMapper donationRecordMapper;
+
     @Override
     public boolean createDonationRecord(DonationRecord donationRecord) {
-        return false;
+        try {
+            DonationRecordJpaEntity donationRecordJpaEntity = donationRecordMapper.toJpaEntity(donationRecord);
+            donationRecordRepository.save(donationRecordJpaEntity);
+            return true;
+        }catch (Exception e) {
+            return false;
+        }
     }
 }
