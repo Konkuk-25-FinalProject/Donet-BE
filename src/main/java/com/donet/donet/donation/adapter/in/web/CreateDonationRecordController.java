dc62d3d89d7ad19f4e4b581701dd965853626df4
imscow11253 <seaweed8759@naver.com>
2025-10-26 16:23:46 +0900

    [Feat] #26 기부 기록 등록을 위한 Controller 클래스 생성

---
+++ src/main/java/com/donet/donet/donation/adapter/in/web/CreateDonationRecordController.java
@@ -0,0 +1,43 @@
+package com.donet.donet.donation.adapter.in.web;
+
+import com.donet.donet.donation.adapter.in.web.dto.CreateDonationRecordRequest;
+import com.donet.donet.donation.application.port.in.AddDonationRecordUsecase;
+import com.donet.donet.donation.application.port.in.command.AddDonationRecordCommand;
+import com.donet.donet.global.annotation.CurrentUserId;
+import com.donet.donet.global.response.BaseResponse;
+import com.donet.donet.global.swagger.CustomExceptionDescription;
+import io.swagger.v3.oas.annotations.Operation;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import static com.donet.donet.global.swagger.SwaggerResponseDescription.DEFAULT;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/donation")
+public class CreateDonationRecordController implements DonationController{
+    private final AddDonationRecordUsecase addDonationRecordUsecase;
+
+    @Operation(
+            summary = "기부 기록 생성 API",
+            description = """
+                    기부하기 이후에 기부 기록을 생성하기 위한 API입니다. 
+                    """
+    )
+    @CustomExceptionDescription(DEFAULT)
+    @GetMapping("/donate")
+    public BaseResponse<Object> createDonationRecord(@CurrentUserId Long userId, @RequestBody CreateDonationRecordRequest createDonationRecordRequest) {
+        AddDonationRecordCommand command = new AddDonationRecordCommand(
+                userId,
+                createDonationRecordRequest.donationId(),
+                createDonationRecordRequest.donationAmount(),
+                createDonationRecordRequest.isAnonymous(),
+                createDonationRecordRequest.walletAddress()
+        );
+
+        return new BaseResponse<>(addDonationRecordUsecase.addDonationRecord(command));
+    }
+}
