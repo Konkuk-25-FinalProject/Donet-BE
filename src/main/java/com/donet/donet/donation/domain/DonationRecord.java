caeaf659436453491575f5800241427ca4ce4b9d
imscow11253 <seaweed8759@naver.com>
2025-10-26 15:19:19 +0900

    [Feat] #24 기부 기록 도메인 엔티티 클래스 생성

---
+++ src/main/java/com/donet/donet/donation/domain/DonationRecord.java
@@ -0,0 +1,9 @@
+package com.donet.donet.donation.domain;
+
+public class DonationRecord {
+    private Long id;
+    private Long donationAmount;
+    private Long userId;
+    private Long donationId;
+    private String walletAddress;
+}
