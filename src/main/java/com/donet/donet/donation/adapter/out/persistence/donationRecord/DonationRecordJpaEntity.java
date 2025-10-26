4c82a9e2892866af74b3039fb17199e78af8331e
imscow11253 <seaweed8759@naver.com>
2025-10-26 15:41:17 +0900

    [Feat] #25 기부 기록 Jpa Entity 클래스 생성

---
+++ src/main/java/com/donet/donet/donation/adapter/out/persistence/donationRecord/DonationRecordJpaEntity.java
@@ -0,0 +1,31 @@
+package com.donet.donet.donation.adapter.out.persistence.donationRecord;
+
+import com.donet.donet.donation.adapter.out.persistence.donation.DonationJpaEntity;
+import com.donet.donet.user.adapter.out.persistence.UserJpaEntity;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Table(name = "donation_record")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Entity
+public class DonationRecordJpaEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private Long donationAmount;
+
+    @ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    private UserJpaEntity userJpaEntity;
+
+    @ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    private DonationJpaEntity donationJpaEntity;
+
+//    @ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+//    private WalletJpaEntity walletJpaEntity;
+}
