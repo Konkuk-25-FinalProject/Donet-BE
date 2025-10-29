47c47ef023a80d4efca5dcf83699dc0a2629fe43
JangIkhwan <bluensky0213@gmail.com>
2025-10-29 21:37:33 +0900

    [Feat] #33 기부후기 도메인 엔티티 작성
    - 기부 후기 도메인 엔티티 작성

---
+++ src/main/java/com/donet/donet/review/domain/DonationReview.java
@@ -0,0 +1,22 @@
+package com.donet.donet.review.domain;
+
+import com.donet.donet.review.application.port.in.CreateDonationReviewCommand;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.util.List;
+
+@Getter
+@AllArgsConstructor
+public class DonationReview {
+    private Long id;
+    private String title;
+    private String summary;
+    private List<String> tags;
+    private String imageUrl;
+    private String content;
+
+    public DonationReview(CreateDonationReviewCommand command, String imageUrl) {
+        this(null, command.getTitle(), command.getSummary(), command.getTags(), imageUrl, command.getContent());
+    }
+}
