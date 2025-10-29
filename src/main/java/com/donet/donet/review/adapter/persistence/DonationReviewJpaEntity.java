3d3963b5186e15ccd8e93a8a7604d6671fdfde6b
JangIkhwan <bluensky0213@gmail.com>
2025-10-29 21:51:33 +0900

    [Feat] #33 기부후기 JPA 엔티티와 리포지토리 작성
    - 기부 후기 JPA 엔티티와 리포지토리 작성

---
+++ src/main/java/com/donet/donet/review/adapter/persistence/DonationReviewJpaEntity.java
@@ -0,0 +1,25 @@
+package com.donet.donet.review.adapter.persistence;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class DonationReviewJpaEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+
+    private String summary;
+
+    @Lob
+    private String content;
+
+    private String imageUrl;
+}
---
+++ src/main/java/com/donet/donet/review/adapter/persistence/DonationReviewRepository.java
@@ -0,0 +1,6 @@
+package com.donet.donet.review.adapter.persistence;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DonationReviewRepository extends JpaRepository<DonationReviewJpaEntity, Long> {
+}
