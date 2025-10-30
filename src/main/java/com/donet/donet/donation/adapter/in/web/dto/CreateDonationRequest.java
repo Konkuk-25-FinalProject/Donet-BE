b7f3579d22308db6a015acc9907e06e710222c05
imscow11253 <seaweed8759@naver.com>
2025-10-30 19:37:21 +0900

    [Feat] #29 HTTP Request DTO 클래스에 Command로 변경하는 메소드 추가

--- src/main/java/com/donet/donet/donation/adapter/in/web/dto/CreateDonationRequest.java
+++ src/main/java/com/donet/donet/donation/adapter/in/web/dto/CreateDonationRequest.java
@@ -1,7 +1,10 @@
 package com.donet.donet.donation.adapter.in.web.dto;
 
+import com.donet.donet.donation.application.port.in.command.CreateDonationCommand;
+
 import java.time.LocalDate;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public record CreateDonationRequest(
         String title,
@@ -19,5 +22,11 @@ public record CreateDonationRequest(
             Long amount,
             Long price
     ){
+    }
+
+    public List<CreateDonationCommand.Item> toCommandItem() {
+        return this.items.stream()
+                .map(item -> new CreateDonationCommand.Item(item.itemName, item.amount, item.price))
+                .toList();
     }
 }
