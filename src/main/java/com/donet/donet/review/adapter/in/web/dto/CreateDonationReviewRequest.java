2e76357d5451a846d2de1ffa8d559f11e96f1cc5
JangIkhwan <bluensky0213@gmail.com>
2025-10-30 16:14:11 +0900

    [Feat] #33 웹어댑터 입력 모델에 필드 추가
    - 컨트롤러에서 태그 리스트를 받을 수 있도록 필드 추가

--- src/main/java/com/donet/donet/review/adapter/in/web/dto/CreateDonationReviewRequest.java
+++ src/main/java/com/donet/donet/review/adapter/in/web/dto/CreateDonationReviewRequest.java
@@ -4,12 +4,15 @@ package com.donet.donet.review.adapter.in.web.dto;
 import com.donet.donet.review.application.port.in.CreateDonationReviewCommand;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.util.List;
+
 public record CreateDonationReviewRequest (
         String title,
         String summary,
+        List<String> tags,
         String content
 ){
     public CreateDonationReviewCommand toCommand(MultipartFile reviewImage) {
-        return new CreateDonationReviewCommand(title, summary, null, content, reviewImage);
+        return new CreateDonationReviewCommand(title, summary, tags, content, reviewImage);
     }
 }
