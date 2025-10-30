f4dcefe4e4c578d2e8d1a4ce2eb260e453c9a061
imscow11253 <seaweed8759@naver.com>
2025-10-30 21:17:24 +0900

    [Feat] #38 기부별 카테고리 저장 메소드 생성

--- src/main/java/com/donet/donet/donation/adapter/out/persistence/category/CategoriesRepository.java
+++ src/main/java/com/donet/donet/donation/adapter/out/persistence/category/CategoriesRepository.java
@@ -1,8 +1,10 @@
 package com.donet.donet.donation.adapter.out.persistence.category;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -18,4 +20,19 @@ public interface CategoriesRepository extends JpaRepository<CategoryJpaEntity, L
             nativeQuery = true
     )
     List<CategoryJpaEntity> findCategoriesByName(@Param("names") List<String> names);
+
+    @Query(
+            value = "SELECT COUNT(DISTINCT name) = :#{#names.size()} FROM category WHERE name IN (:names)",
+            nativeQuery = true
+    )
+    boolean existsByName(@Param("names") List<String> names);
+
+    @Modifying
+    @Transactional
+    @Query(
+            value = "INSERT INTO DonationCategory (donationId, categoryId) " +
+                    "SELECT :donationId, c.id FROM Category c WHERE c.name IN (:categoryNames)",
+            nativeQuery = true
+    )
+    void saveDonationCategory(@Param("donationId") Long donationId, @Param("categoryNames") List<String> categoryNames);
 }
