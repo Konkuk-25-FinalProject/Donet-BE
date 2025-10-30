fb4a3ee616ef46d05d5f174537eb065275985039
JangIkhwan <bluensky0213@gmail.com>
2025-10-30 21:44:15 +0900

    [Feat] #33 TestDataFactory에 메서드 추가
    - 기부글을 작성한 사용자를 생성하는 메서드 추가

--- src/test/java/com/donet/donet/global/util/TestDataFactory.java
+++ src/test/java/com/donet/donet/global/util/TestDataFactory.java
@@ -1,16 +1,33 @@
 package com.donet.donet.global.util;
 
+import com.donet.donet.donation.adapter.out.persistence.PartnerJpaEntity;
+import com.donet.donet.donation.adapter.out.persistence.donation.DonationJpaEntity;
+import com.donet.donet.donation.adapter.out.persistence.donation.DonationRepository;
 import com.donet.donet.user.adapter.out.persistence.UserJpaEntity;
 import com.donet.donet.user.adapter.out.persistence.UserRepository;
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
 
 @Component
 public class TestDataFactory {
     @Autowired
     UserRepository userRepository;
 
+    @Autowired
+    DonationRepository donationRepository;
+
     public UserJpaEntity createUser() {
         return userRepository.save(new UserJpaEntity(null, "닉네임", "이미지",  "KAKAO", "kakaocode", "waller"));
+    }
+
+    @Transactional
+    public UserJpaEntity createUserWhoWroteDonation() {
+        UserJpaEntity user = userRepository.save(new UserJpaEntity(null, "닉네임", "이미지", "KAKAO", "kakaocode", "waller"));
+        PartnerJpaEntity partner = new PartnerJpaEntity(null, "파트너사", "지갑주소");
+        donationRepository.save(new DonationJpaEntity(null, "제목", true, LocalDate.now(), LocalDate.now(), "설명", 1000L, 1000L, 0L, user, partner));
+        return user;
     }
 }
