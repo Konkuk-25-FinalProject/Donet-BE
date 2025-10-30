eba5ecdfa3967d819c21598223e1b875c87c8b44
imscow11253 <seaweed8759@naver.com>
2025-10-30 16:54:51 +0900

    [Chore] #27 DonationMapper에서 Repository 의존성 없애기 위한 TODO 주석 작성

--- src/main/java/com/donet/donet/donation/adapter/out/persistence/donation/DonationMapper.java
+++ src/main/java/com/donet/donet/donation/adapter/out/persistence/donation/DonationMapper.java
@@ -9,6 +9,7 @@ import java.util.List;
 @Component
 @RequiredArgsConstructor
 public class DonationMapper {
+    //TODO: 의존성 없애기 --> Adapter에서 조회해서 인자로 넘겨주는 방식으로 변경할 것
     private final DonationImageRepository donationImageRepository;
 
     public Donation mapToDomainEntity(DonationJpaEntity donationJpaEntity) {
