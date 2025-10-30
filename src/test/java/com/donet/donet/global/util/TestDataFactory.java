b46ad8706464ab6d9ba1df4a607ff2fc0705f3dc
JangIkhwan <bluensky0213@gmail.com>
2025-10-31 00:27:58 +0900

    [Test] #33 DB 데이터 생성 메서드 수정
    - 기부 엔티티에서 연관관계 수정에 따라 DB 데이터 생성 메서드 수정

--- src/test/java/com/donet/donet/global/util/TestDataFactory.java
+++ src/test/java/com/donet/donet/global/util/TestDataFactory.java
@@ -1,6 +1,7 @@
 package com.donet.donet.global.util;
 
 import com.donet.donet.donation.adapter.out.persistence.PartnerJpaEntity;
+import com.donet.donet.donation.adapter.out.persistence.PartnerRepository;
 import com.donet.donet.donation.adapter.out.persistence.donation.DonationJpaEntity;
 import com.donet.donet.donation.adapter.out.persistence.donation.DonationRepository;
 import com.donet.donet.user.adapter.out.persistence.UserJpaEntity;
@@ -17,6 +18,9 @@ public class TestDataFactory {
     UserRepository userRepository;
 
     @Autowired
+    PartnerRepository partnerRepository;
+
+    @Autowired
     DonationRepository donationRepository;
 
     public UserJpaEntity createUser() {
@@ -26,7 +30,7 @@ public class TestDataFactory {
     @Transactional
     public UserJpaEntity createUserWhoWroteDonation() {
         UserJpaEntity user = userRepository.save(new UserJpaEntity(null, "닉네임", "이미지", "KAKAO", "kakaocode", "waller"));
-        PartnerJpaEntity partner = new PartnerJpaEntity(null, "파트너사", "지갑주소");
+        PartnerJpaEntity partner = partnerRepository.save(new PartnerJpaEntity(null, "파트너사", "지갑주소"));
         donationRepository.save(new DonationJpaEntity(null, "제목", true, LocalDate.now(), LocalDate.now(), "설명", 1000L, 1000L, 0L, user, partner));
         return user;
     }
