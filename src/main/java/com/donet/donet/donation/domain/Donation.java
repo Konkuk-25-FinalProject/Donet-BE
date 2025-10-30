67cfb8b24835070f31346b1f69860d41c7c8b549
imscow11253 <seaweed8759@naver.com>
2025-10-30 16:39:18 +0900

    [Update] #27 기부 도메인 엔티티에 Builder 어노테이션 추가

--- src/main/java/com/donet/donet/donation/domain/Donation.java
+++ src/main/java/com/donet/donet/donation/domain/Donation.java
@@ -1,12 +1,14 @@
 package com.donet.donet.donation.domain;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 
 import java.time.LocalDate;
 import java.util.List;
 
 @Getter
+@Builder
 @AllArgsConstructor
 public class Donation {
     private Long id;
