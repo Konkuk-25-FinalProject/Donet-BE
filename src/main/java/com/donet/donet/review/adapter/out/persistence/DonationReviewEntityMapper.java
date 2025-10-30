517b7e3d53a861904413d1df5dd453c4b45e725b
JangIkhwan <bluensky0213@gmail.com>
2025-10-30 15:57:31 +0900

    [Feat] #33 기부후기 엔티티 매퍼 수정
    - 태그 필드를 매핑하는 로직 추가

--- src/main/java/com/donet/donet/review/adapter/out/persistence/DonationReviewEntityMapper.java
+++ src/main/java/com/donet/donet/review/adapter/out/persistence/DonationReviewEntityMapper.java
@@ -2,11 +2,15 @@ package com.donet.donet.review.adapter.out.persistence;
 
 import com.donet.donet.review.domain.DonationReview;
 
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
 public class DonationReviewEntityMapper {
     public static DonationReviewJpaEntity mapToJpaEntity(DonationReview entity) {
         return new DonationReviewJpaEntity(entity.getId(),
                 entity.getTitle(),
                 entity.getSummary(),
+                entity.getTags().stream().collect(Collectors.joining("#")),
                 entity.getContent(),
                 entity.getImageUrl());
     }
@@ -15,6 +19,7 @@ public class DonationReviewEntityMapper {
         return new DonationReview(jpaEntity.getId(),
                 jpaEntity.getTitle(),
                 jpaEntity.getSummary(),
+                Arrays.stream(jpaEntity.getTags().split("#")).toList(),
                 jpaEntity.getImageUrl(),
                 jpaEntity.getContent());
     }
