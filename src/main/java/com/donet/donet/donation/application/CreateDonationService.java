8352a84603053e24cb1248a3702cc8dad801a966
imscow11253 <seaweed8759@naver.com>
2025-10-30 17:00:02 +0900

    [Feat] #27 기부 생성 usecase 구현체 Service 클래스 로직 구현

--- src/main/java/com/donet/donet/donation/application/CreateDonationService.java
+++ src/main/java/com/donet/donet/donation/application/CreateDonationService.java
@@ -3,9 +3,13 @@ package com.donet.donet.donation.application;
 import com.donet.donet.donation.application.port.in.CreateDonationUsecase;
 import com.donet.donet.donation.application.port.in.command.CreateDonationCommand;
 import com.donet.donet.donation.application.port.out.CreateDonationPort;
+import com.donet.donet.donation.domain.Donation;
+import com.donet.donet.donation.domain.DonationItem;
 import com.donet.donet.user.application.port.out.ImageUploaderPort;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -15,9 +19,36 @@ public class CreateDonationService implements CreateDonationUsecase {
 
     @Override
     public void createDonation(CreateDonationCommand command) {
-        //Donation 객체 만들기
+        List<String> imagesUrls = command.images()
+                .stream()
+                .map(imageUploaderPort::upload)
+                .toList();
 
-        //port 메소드 호출
+        List<DonationItem> donationItems = command.items()
+                .stream()
+                .map(item -> DonationItem.builder()
+                        .name(item.itemName())
+                        .quantity(item.amount())
+                        .price(item.price())
+                        .build()
+                )
+                .toList();
 
+        Donation donation = Donation.builder()
+                .title(command.title())
+                .description(command.content())
+                .anonymous(command.isAnonymous())
+                .startDate(command.startDate())
+                .endDate(command.endDate())
+                .targetAmount(command.targetAmount())
+                .currentAmount(0L)
+                .views(0L)
+                .imageUrl(imagesUrls)
+                .userId(command.userId())
+                .partnerId(command.partnerId())
+                .donationItems(donationItems)
+                .build();
+
+        createDonationPort.createDonation(donation);
     }
 }
