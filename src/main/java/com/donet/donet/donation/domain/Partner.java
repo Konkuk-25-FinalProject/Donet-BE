b3b3ce5e51a8a5ca60ca3e354f1ce3f8f304d030
imscow11253 <seaweed8759@naver.com>
2025-10-30 22:00:01 +0900

    [Feat] #44 파트너사 도메인 엔티티 생성

---
+++ src/main/java/com/donet/donet/donation/domain/Partner.java
@@ -0,0 +1,10 @@
+package com.donet.donet.donation.domain;
+
+import lombok.Getter;
+
+@Getter
+public class Partner {
+    private Long id;
+    private String name;
+    private String walletAddress;
+}
