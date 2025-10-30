75059aaad7fa9aab284b0f8132b141823aa28f79
imscow11253 <seaweed8759@naver.com>
2025-10-30 21:50:13 +0900

    [Feat] #44 파트너사 조회 usecase 인터페이스 생성

---
+++ src/main/java/com/donet/donet/donation/application/port/in/GetPartnerUsecase.java
@@ -0,0 +1,7 @@
+package com.donet.donet.donation.application.port.in;
+
+import com.donet.donet.donation.application.port.in.dto.response.GetPartnerResponse;
+
+public interface GetPartnerUsecase {
+    GetPartnerResponse getPartner();
+}
