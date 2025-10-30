d3973adf05f3f080b3c4d99303c0bf7c6c097e2a
JangIkhwan <bluensky0213@gmail.com>
2025-10-30 16:24:19 +0900

    [Docs] #33 기부후기 생성 API 명세 작성
    - 기부후기 생성 컨트롤러에 명세 추가

--- src/main/java/com/donet/donet/review/adapter/in/web/CreateDonationReviewController.java
+++ src/main/java/com/donet/donet/review/adapter/in/web/CreateDonationReviewController.java
@@ -1,8 +1,10 @@
 package com.donet.donet.review.adapter.in.web;
 
 import com.donet.donet.global.response.BaseResponse;
+import com.donet.donet.global.swagger.CustomExceptionDescription;
 import com.donet.donet.review.adapter.in.web.dto.CreateDonationReviewRequest;
 import com.donet.donet.review.application.port.in.CreateDonationReviewUsecase;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,11 +12,19 @@ import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import static com.donet.donet.global.swagger.SwaggerResponseDescription.CREATE_DONATION_REVIEW;
+
 @RequiredArgsConstructor
 @RequestMapping("reviews")
 @RestController
-public class CreateDonationReviewController {
+public class CreateDonationReviewController implements DonationReviewController{
     private final CreateDonationReviewUsecase createDonationReviewUsecase;
+
+    @Operation(
+            summary = "기부후기 생성",
+            description = "기부후기를 생성할 수 있다. 기부후기에서 이미지는 선택사항이다."
+    )
+    @CustomExceptionDescription(CREATE_DONATION_REVIEW)
     @PostMapping
     public BaseResponse<Void> createDonationReview(@RequestPart CreateDonationReviewRequest reviewRequest,
                                                    @RequestPart(required = false) MultipartFile reviewImage){
