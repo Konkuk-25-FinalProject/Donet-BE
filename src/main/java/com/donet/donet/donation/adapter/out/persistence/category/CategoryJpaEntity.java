5f9fde3fdbef8a425928ecc8472fc0afdf04f44c
imscow11253 <seaweed8759@naver.com>
2025-11-02 19:06:01 +0900

    [Refactor] #38 카테고리 Jpa 엔티티에 중간 테이블 엔티티를 일대다 관계로 추가

--- src/main/java/com/donet/donet/donation/adapter/out/persistence/category/CategoryJpaEntity.java
+++ src/main/java/com/donet/donet/donation/adapter/out/persistence/category/CategoryJpaEntity.java
@@ -1,10 +1,14 @@
 package com.donet.donet.donation.adapter.out.persistence.category;
 
+import com.donet.donet.donation.adapter.out.persistence.donationCategory.DonationCategoryJpaEntity;
 import com.donet.donet.global.persistence.BaseEntity;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Table(name = "category")
 @NoArgsConstructor
@@ -18,4 +22,7 @@ public class CategoryJpaEntity extends BaseEntity {
 
     @Column(nullable = false)
     private String name;
+
+    @OneToMany(mappedBy = "category", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<DonationCategoryJpaEntity> donationCategories = new ArrayList<>();
 }
