5e14d422b6692a49b7b6bd6efc32846a2c5ff724
JangIkhwan <bluensky0213@gmail.com>
2025-11-03 17:26:45 +0900

    [Fix] #33 테스트 데이터 생성 메서드 수정
    - 테스트 데이터 생성 메서드 수정

--- src/test/java/com/donet/donet/global/util/TestDataFactory.java
+++ src/test/java/com/donet/donet/global/util/TestDataFactory.java
@@ -1,9 +1,14 @@
 package com.donet.donet.global.util;
 
-import com.donet.donet.donation.adapter.out.persistence.PartnerJpaEntity;
-import com.donet.donet.donation.adapter.out.persistence.PartnerRepository;
+import com.donet.donet.donation.adapter.out.persistence.category.CategoriesRepository;
+import com.donet.donet.donation.adapter.out.persistence.category.CategoryJpaEntity;
 import com.donet.donet.donation.adapter.out.persistence.donation.DonationJpaEntity;
 import com.donet.donet.donation.adapter.out.persistence.donation.DonationRepository;
+import com.donet.donet.donation.adapter.out.persistence.donationCategory.DonationCategoryJpaEntity;
+import com.donet.donet.donation.adapter.out.persistence.donationCategory.DonationCategoryRepository;
+import com.donet.donet.donation.adapter.out.persistence.donationItem.DonationItemJpaEntity;
+import com.donet.donet.donation.adapter.out.persistence.partner.PartnerJpaEntity;
+import com.donet.donet.donation.adapter.out.persistence.partner.PartnerRepository;
 import com.donet.donet.user.adapter.out.persistence.UserJpaEntity;
 import com.donet.donet.user.adapter.out.persistence.UserRepository;
 import jakarta.transaction.Transactional;
@@ -11,6 +16,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 @Component
 public class TestDataFactory {
@@ -23,6 +30,12 @@ public class TestDataFactory {
     @Autowired
     DonationRepository donationRepository;
 
+    @Autowired
+    CategoriesRepository categoriesRepository;
+
+    @Autowired
+    DonationCategoryRepository donationCategoryRepository;
+
     public UserJpaEntity createUser() {
         return userRepository.save(new UserJpaEntity(null, "닉네임", "이미지",  "KAKAO", "kakaocode", "waller"));
     }
@@ -31,7 +44,23 @@ public class TestDataFactory {
     public UserJpaEntity createUserWhoWroteDonation() {
         UserJpaEntity user = userRepository.save(new UserJpaEntity(null, "닉네임", "이미지", "KAKAO", "kakaocode", "waller"));
         PartnerJpaEntity partner = partnerRepository.save(new PartnerJpaEntity(null, "파트너사", "지갑주소"));
-        donationRepository.save(new DonationJpaEntity(null, "제목", true, LocalDate.now(), LocalDate.now(), "설명", 1000L, 1000L, 0L, user, partner));
+        DonationItemJpaEntity donationItem = new DonationItemJpaEntity(null, "삼다수", 1L, 5000L, null);
+
+        DonationJpaEntity donation = donationRepository.save(new DonationJpaEntity(null, "제목", true,
+                LocalDate.now(), LocalDate.now(),
+                "설명", 1000L, 1000L, 0L,
+                user,
+                partner,
+                new ArrayList<>(List.of(donationItem)),
+                new ArrayList<>(),
+                new ArrayList<>(List.of())));
+
+        CategoryJpaEntity category = categoriesRepository.save(new CategoryJpaEntity(null, "생필품", null));
+        DonationCategoryJpaEntity donationCategory = new DonationCategoryJpaEntity(null, donation, category);
+        donationCategoryRepository.save(donationCategory);
+
+        donationRepository.save(donation);
+
         return user;
     }
 }
