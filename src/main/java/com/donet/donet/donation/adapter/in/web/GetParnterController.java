29c991d0721d96f0e074016bf5787130a81833ce
imscow11253 <seaweed8759@naver.com>
2025-11-05 12:15:53 +0900

    [Refactor] #44 파트너사 조회 API에서 Web Adapter가 영속성 Adapter를 바로 의존하도록 수정

--- src/main/java/com/donet/donet/donation/adapter/in/web/GetParnterController.java
+++ src/main/java/com/donet/donet/donation/adapter/in/web/GetParnterController.java
@@ -2,6 +2,8 @@ package com.donet.donet.donation.adapter.in.web;
 
 import com.donet.donet.donation.application.port.in.GetPartnerUsecase;
 import com.donet.donet.donation.application.port.in.dto.response.GetPartnerResponse;
+import com.donet.donet.donation.application.port.out.FindPartnerPort;
+import com.donet.donet.donation.domain.Partner;
 import com.donet.donet.global.response.BaseResponse;
 import com.donet.donet.global.swagger.CustomExceptionDescription;
 import io.swagger.v3.oas.annotations.Operation;
@@ -10,13 +12,15 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 import static com.donet.donet.global.swagger.SwaggerResponseDescription.DEFAULT;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/partners")
 public class GetParnterController implements DonationController{
-    private final GetPartnerUsecase getPartnerUsecase;
+    private final FindPartnerPort findPartnerPort;
 
     @Operation(
             summary = "파트너사 목록 조회 API",
@@ -27,6 +31,7 @@ public class GetParnterController implements DonationController{
     @CustomExceptionDescription(DEFAULT)
     @GetMapping()
     public BaseResponse<GetPartnerResponse> getParnters() {
-        return new BaseResponse<>(getPartnerUsecase.getPartner());
+        List<Partner> partners = findPartnerPort.findAllPartners();
+        return new BaseResponse<>(GetPartnerResponse.from(partners));
     }
 }
