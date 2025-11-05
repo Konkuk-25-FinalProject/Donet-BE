a34d69ffed56356084c5d0009fb5d48e359999f5
imscow11253 <seaweed8759@naver.com>
2025-11-05 14:34:53 +0900

    [Feat] #42 DonationReview Repository에서 최신순으로 리뷰를 조회하는 메소드 구현

--- src/main/java/com/donet/donet/review/adapter/out/persistence/DonationReviewRepository.java
+++ src/main/java/com/donet/donet/review/adapter/out/persistence/DonationReviewRepository.java
@@ -1,6 +1,15 @@
 package com.donet.donet.review.adapter.out.persistence;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface DonationReviewRepository extends JpaRepository<DonationReviewJpaEntity, Long> {
+    @Query(
+            value = "SELECT * FROM donation_review dr ORDER BY dr.create_at DESC LIMIT :limit",
+            nativeQuery = true
+    )
+    List<DonationReviewJpaEntity> findRecentReviews(@Param("limit") Integer limit);
 }
