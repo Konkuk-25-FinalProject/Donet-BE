da168eff955973ed8968b984dd8ff460c2da325e
imscow11253 <seaweed8759@naver.com>
2025-11-05 13:48:40 +0900

    [Feat] #41 메인 페이지 조회 http Response Record 클래스에 정적 팩토리 메소드 추가

--- src/main/java/com/donet/donet/donation/application/port/in/dto/response/GetMainPageInfoResponse.java
+++ src/main/java/com/donet/donet/donation/application/port/in/dto/response/GetMainPageInfoResponse.java
@@ -1,5 +1,7 @@
 package com.donet.donet.donation.application.port.in.dto.response;
 
+import com.donet.donet.donation.domain.Donation;
+
 import java.util.List;
 
 public record GetMainPageInfoResponse (
@@ -13,19 +15,53 @@ public record GetMainPageInfoResponse (
             String title,
             String content,
             Long raised
-    ){}
+    ){
+        public static TopDonation fromDonation (Donation donation) {
+            return new TopDonation(
+                    donation.getId(), donation.getImageUrl().get(0), donation.getTitle(), donation.getDescription(), donation.getCurrentAmount()
+            );
+        }
+    }
 
     public record RecommendDonation(
             Long donationId,
             String imgUrl,
             String title,
             Long raised
-    ){}
+    ){
+        public static RecommendDonation fromDonation (Donation donation) {
+            return new RecommendDonation(
+                    donation.getId(), donation.getImageUrl().get(0), donation.getTitle(), donation.getCurrentAmount()
+            );
+        }
+    }
 
     public record DonationReview(
             Long reviewId,
             String title,
             String content,
             String imgUrl
-    ){}
+    ){
+        public static GetMainPageInfoResponse.DonationReview fromDonationReview (DonationReview review) {
+            return new GetMainPageInfoResponse.DonationReview(
+                    review.reviewId, review.title, review.content, review.imgUrl
+            );
+        }
+    }
+
+    public static GetMainPageInfoResponse from(List<Donation> topDonations, List<Donation> recommendDonations, List<DonationReview> donationReviews) {
+        List<TopDonation> topDonationsResponse = topDonations.stream()
+                .map(TopDonation::fromDonation)
+                .toList();
+
+        List<RecommendDonation> recommendDonationsResponse = recommendDonations.stream()
+                .map(RecommendDonation::fromDonation)
+                .toList();
+
+        List<GetMainPageInfoResponse.DonationReview> donationReviewsResponse = donationReviews.stream()
+                .map(GetMainPageInfoResponse.DonationReview::fromDonationReview)
+                .toList();
+
+        return new GetMainPageInfoResponse(topDonationsResponse, recommendDonationsResponse, donationReviewsResponse);
+    }
 }
