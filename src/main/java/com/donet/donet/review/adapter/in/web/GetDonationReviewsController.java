c7fdbb7460e32668d4dccc36d27a737a1981afba
JangIkhwan <bluensky0213@gmail.com>
2025-11-05 20:45:42 +0900

    [Docs] #52 기부후기 리스트 조회 API 명세 작성
    - API 스웨거 명세 작성

--- src/main/java/com/donet/donet/review/adapter/in/web/GetDonationReviewsController.java
+++ src/main/java/com/donet/donet/review/adapter/in/web/GetDonationReviewsController.java
@@ -1,8 +1,11 @@
 package com.donet.donet.review.adapter.in.web;
 
 import com.donet.donet.global.response.BaseResponse;
+import com.donet.donet.global.swagger.CustomExceptionDescription;
+import com.donet.donet.global.swagger.SwaggerResponseDescription;
 import com.donet.donet.review.application.port.in.GetDonationReviewsUsecase;
 import com.donet.donet.review.application.port.in.dto.GetDonationReviewsResponse;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,9 +17,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 @RequestMapping("reviews")
 @RestController
-public class GetDonationReviewsController {
+public class GetDonationReviewsController implements DonationReviewController{
     private final GetDonationReviewsUsecase getDonationReviewsUsecase;
 
+    @Operation(
+            summary = "기부후기 리스트 조회",
+            description = "기부후기 리스트를 조회할 수 있다"
+    )
+    @CustomExceptionDescription(SwaggerResponseDescription.GET_DONATION_REVIEW)
     @GetMapping
     public BaseResponse<GetDonationReviewsResponse> getDonationReviews(@RequestParam("size") int size, Long lastId){
         log.info("[getDonationReviews] size = {}, lastId = {}", size, lastId);
