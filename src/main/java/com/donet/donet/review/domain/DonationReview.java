4fce3735f93a270b2d0c619a81c3f85014cae371
JangIkhwan <bluensky0213@gmail.com>
2025-11-05 15:02:15 +0900

    [Fix] #47 기부후기에서 User를 필드로 갖도록 수정
    - 작성자를 필드로 갖도록 수정

--- src/main/java/com/donet/donet/review/domain/DonationReview.java
+++ src/main/java/com/donet/donet/review/domain/DonationReview.java
@@ -1,6 +1,7 @@
 package com.donet.donet.review.domain;
 
 import com.donet.donet.review.application.port.in.dto.CreateDonationReviewCommand;
+import com.donet.donet.user.domain.User;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
@@ -11,13 +12,13 @@ import java.util.List;
 public class DonationReview {
     private Long id;
     private String title;
-    private String writerName;
+    private User writer;
     private String summary;
     private List<String> tags;
     private String imageUrl;
     private String content;
 
     public DonationReview(CreateDonationReviewCommand command, String imageUrl) {
-        this(null, command.getTitle(),null, command.getSummary(), command.getTags(), imageUrl, command.getContent());
+        this(null, command.getTitle(), new User(command.getUserId()), command.getSummary(), command.getTags(), imageUrl, command.getContent());
     }
 }
