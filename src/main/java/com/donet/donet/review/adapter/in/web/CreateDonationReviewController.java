a4f0ee1d7308be1d68d1597b2a9470aa624c8cc6
JangIkhwan <bluensky0213@gmail.com>
2025-11-05 13:07:34 +0900

    [Feat] #33 기부후기 생성 API 요청 파라미터 검증 추가
    - 요청 파라미터에 빈 문자열이 들어올 수 없도록 수정

--- src/main/java/com/donet/donet/review/adapter/in/web/CreateDonationReviewController.java
+++ src/main/java/com/donet/donet/review/adapter/in/web/CreateDonationReviewController.java
@@ -9,6 +9,7 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestPart;
@@ -30,7 +31,7 @@ public class CreateDonationReviewController implements DonationReviewController{
     @CustomExceptionDescription(CREATE_DONATION_REVIEW)
     @PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     public BaseResponse<Void> createDonationReview(@Parameter(hidden = true) @CurrentUserId Long userId,
-                                                   @Parameter @RequestPart CreateDonationReviewRequest reviewRequest,
+                                                   @Parameter @RequestPart @Validated CreateDonationReviewRequest reviewRequest,
                                                    @Parameter @RequestPart(required = false) MultipartFile reviewImage){
         createDonationReviewUsecase.create(reviewRequest.toCommand(userId, reviewImage));
         return new BaseResponse<>(null);
--- src/main/java/com/donet/donet/review/adapter/in/web/dto/CreateDonationReviewRequest.java
+++ src/main/java/com/donet/donet/review/adapter/in/web/dto/CreateDonationReviewRequest.java
@@ -2,16 +2,17 @@ package com.donet.donet.review.adapter.in.web.dto;
 
 
 import com.donet.donet.review.application.port.in.dto.CreateDonationReviewCommand;
+import jakarta.validation.constraints.NotBlank;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 
 public record CreateDonationReviewRequest (
         Long donationId,
-        String title,
-        String summary,
-        List<String> tags,
-        String content
+        @NotBlank String title,
+        @NotBlank String summary,
+        List<@NotBlank String> tags,
+        @NotBlank String content
 ){
     public CreateDonationReviewCommand toCommand(Long userId, MultipartFile reviewImage) {
         return new CreateDonationReviewCommand(userId, donationId, title, summary, tags, content, reviewImage);
