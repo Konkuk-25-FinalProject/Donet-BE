05639f550cd38b8a1df6009952ebe235ca98f9ca
imscow11253 <seaweed8759@naver.com>
2025-11-05 14:07:12 +0900

    [Refactor] #42 사용하지 않는 메소드 삭제

--- src/main/java/com/donet/donet/donation/adapter/out/persistence/category/CategoriesRepository.java
+++ src/main/java/com/donet/donet/donation/adapter/out/persistence/category/CategoriesRepository.java
@@ -21,18 +21,4 @@ public interface CategoriesRepository extends JpaRepository<CategoryJpaEntity, L
     )
     List<CategoryJpaEntity> findCategoriesByName(@Param("names") List<String> names);
 
-    @Query(
-            value = "SELECT COUNT(DISTINCT name) = :size FROM category WHERE name IN (:names)",
-            nativeQuery = true
-    )
-    boolean existsByName(@Param("names") List<String> names, @Param("size") int size);
-
-    @Modifying
-    @Transactional
-    @Query(
-            value = "INSERT INTO donation_category (donation_id, category_id) " +
-                    "SELECT :donationId, c.id FROM category c WHERE c.name IN (:categoryNames)",
-            nativeQuery = true
-    )
-    void saveDonationCategory(@Param("donationId") Long donationId, @Param("categoryNames") List<String> categoryNames);
 }
