133f917f7b662359fb0adc53936b6f8705c5e78c
imscow11253 <seaweed8759@naver.com>
2025-11-09 16:34:09 +0900

    [Fix] #56 기부 내역 생성시 Donation의 currentAmount 필드값 수정 로직 추가

--- src/main/java/com/donet/donet/donation/application/AddDonationRecordService.java
+++ src/main/java/com/donet/donet/donation/application/AddDonationRecordService.java
@@ -3,6 +3,9 @@ package com.donet.donet.donation.application;
 import com.donet.donet.donation.application.port.in.AddDonationRecordUsecase;
 import com.donet.donet.donation.application.port.in.dto.command.AddDonationRecordCommand;
 import com.donet.donet.donation.application.port.out.CreateDonationRecordPort;
+import com.donet.donet.donation.application.port.out.FindDonationPort;
+import com.donet.donet.donation.application.port.out.UpdateDonationPort;
+import com.donet.donet.donation.domain.Donation;
 import com.donet.donet.donation.domain.DonationRecord;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -12,9 +15,12 @@ import org.springframework.stereotype.Service;
 public class AddDonationRecordService implements AddDonationRecordUsecase {
 
     private final CreateDonationRecordPort createDonationRecordPort;
+    private final UpdateDonationPort updateDonationPort;
 
     @Override
     public void addDonationRecord(AddDonationRecordCommand command) {
+        updateDonationPort.addDonationAmount(command.donationId(), command.donationAmount());
+
         DonationRecord donationRecord = DonationRecord.builder()
                 .userId(command.userId())
                 .donationAmount(command.donationAmount())
