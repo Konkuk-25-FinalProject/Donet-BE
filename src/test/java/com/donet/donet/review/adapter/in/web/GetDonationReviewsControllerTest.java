e3b854bc5c6176ae131f0d2a16d7ec04f6c9c869
JangIkhwan <bluensky0213@gmail.com>
2025-11-09 16:39:59 +0900

    [Test] #52 기부후기 리스트 조회 API 테스트 추가
    - lastId가 null인 경우 케이스 작성

--- src/test/java/com/donet/donet/review/adapter/in/web/GetDonationReviewsControllerTest.java
+++ src/test/java/com/donet/donet/review/adapter/in/web/GetDonationReviewsControllerTest.java
@@ -22,7 +22,6 @@ import java.util.stream.IntStream;
 
 import static io.restassured.RestAssured.given;
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
 @ExtendWith(DatabaseClearExtension.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -67,6 +66,44 @@ class GetDonationReviewsControllerTest {
                 .accept(ContentType.JSON)
                 .queryParam("size", size)
                 .queryParam("lastId", 900)
+                .when()
+                .get("reviews")
+                .then()
+                .statusCode(200)
+                .extract()
+                .jsonPath()
+                .getObject("data", GetDonationReviewsResponse.class);
+
+        assertThat(response.reviews()).hasSize(size);
+        assertThat(response.reviews().get(0).title()).isEqualTo(title + 21);
+        assertThat(response.reviews().get(0).writer()).isEqualTo(user.getNickname());
+        assertThat(response.hasNext()).isTrue();
+        assertThat(response.lastId()).isEqualTo(2L);
+    }
+
+    @DisplayName("lastId가 null이어도 기부후기 리스트 조회에 성공한다")
+    @Test
+    void shouldReturnDonationReviewsWhenLastIdIsNull(){
+        int size = 20;
+        String title = "제목";
+        List<String> tags = List.of("태그1", "태그2");
+        String content = "내용";
+
+        UserJpaEntity user = testDataFactory.createUser("KAKAO", "kakaoId1");
+        IntStream.rangeClosed(1, size + 1).forEach(i->{
+            DonationJpaEntity donation = testDataFactory.createDonation(user.getId());
+            testDataFactory.createDonationReview(title + i, tags, content, user.getId(), donation.getId());
+        });
+
+        UserJpaEntity anotherUser = testDataFactory.createUser("KAKAO", "kakaoId2");
+
+        String accessToken = jwtUtil.createAccessToken(anotherUser.getId());
+
+        GetDonationReviewsResponse response = given()
+                .header("Authorization", "Bearer " + accessToken)
+                .contentType(ContentType.JSON)
+                .accept(ContentType.JSON)
+                .queryParam("size", size)
                 .when()
                 .get("reviews")
                 .then()
