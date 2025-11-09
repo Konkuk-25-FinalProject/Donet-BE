3a67735b111b2910d5ff825eedc2edf0d5e2b987
JangIkhwan <bluensky0213@gmail.com>
2025-11-09 14:53:58 +0900

    [Fix] #54 유저 도메인 엔티티에 필드 추가
    - 참여한 기부와 등록한 기부 엔티티에 기부 id를 필드로 추가

--- src/main/java/com/donet/donet/user/domain/JoinedDonation.java
+++ src/main/java/com/donet/donet/user/domain/JoinedDonation.java
@@ -1,6 +1,7 @@
 package com.donet.donet.user.domain;
 
 public record JoinedDonation(
+        Long donationId,
         String title,
         String imageUrl,
         Long amount
--- src/main/java/com/donet/donet/user/domain/RegisteredDonation.java
+++ src/main/java/com/donet/donet/user/domain/RegisteredDonation.java
@@ -3,14 +3,16 @@ package com.donet.donet.user.domain;
 import java.util.List;
 
 public record RegisteredDonation (
+            Long donationId,
             String title,
             String imageUrl,
             Long amount,
             Long progressRate,
             Boolean reviewable
 ){
-    public static RegisteredDonation of(String title, List<String> imageUrls, Long targetAmount, Long currentAmount, boolean reviewable) {
-        return new RegisteredDonation(title,
+    public static RegisteredDonation of(Long donationId, String title, List<String> imageUrls, Long targetAmount, Long currentAmount, boolean reviewable) {
+        return new RegisteredDonation(donationId,
+                title,
                 imageUrls.isEmpty() ? null : imageUrls.get(0),
                 targetAmount,
                 (long)(((double) currentAmount / targetAmount) * 100),
