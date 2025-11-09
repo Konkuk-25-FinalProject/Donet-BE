c007aefc3ac25350b11af9526c11cf00ab761ebf
JangIkhwan <bluensky0213@gmail.com>
2025-11-09 14:56:18 +0900

    [Test] #54 테스트용 기부 기록 생성 메서드 작성
    - 테스트 데이터를 만드는 메서드를 추가

--- src/test/java/com/donet/donet/global/util/TestDataFactory.java
+++ src/test/java/com/donet/donet/global/util/TestDataFactory.java
@@ -7,6 +7,8 @@ import com.donet.donet.donation.adapter.out.persistence.donation.DonationReposit
 import com.donet.donet.donation.adapter.out.persistence.donationCategory.DonationCategoryJpaEntity;
 import com.donet.donet.donation.adapter.out.persistence.donationCategory.DonationCategoryRepository;
 import com.donet.donet.donation.adapter.out.persistence.donationItem.DonationItemJpaEntity;
+import com.donet.donet.donation.adapter.out.persistence.donationRecord.DonationRecordJpaEntity;
+import com.donet.donet.donation.adapter.out.persistence.donationRecord.DonationRecordRepository;
 import com.donet.donet.donation.adapter.out.persistence.partner.PartnerJpaEntity;
 import com.donet.donet.donation.adapter.out.persistence.partner.PartnerRepository;
 import com.donet.donet.review.adapter.out.persistence.DonationReviewJpaEntity;
@@ -42,13 +44,18 @@ public class TestDataFactory {
     @Autowired
     DonationReviewRepository donationReviewRepository;
 
+    @Autowired
+    DonationRecordRepository donationRecordRepository;
+
+    private final String INVALID_PARAMETERS = "테스트 데이터 생성 시 올바르지 않은 인자를 사용";
+
     public UserJpaEntity createUser(String provider, String loginId) {
         return userRepository.save(new UserJpaEntity(null, "닉네임", "이미지",  provider, loginId, "waller"));
     }
 
     @Transactional
     public DonationJpaEntity createDonation(Long userId) {
-        UserJpaEntity user = userRepository.findById(userId).get();
+        UserJpaEntity user = userRepository.findById(userId).orElseThrow(() -> new IllegalArgumentException(INVALID_PARAMETERS));
         PartnerJpaEntity partner = partnerRepository.save(new PartnerJpaEntity(null, "파트너사", "지갑주소"));
         DonationItemJpaEntity donationItem = new DonationItemJpaEntity(null, "삼다수", 1L, 5000L, null);
 
@@ -70,10 +77,35 @@ public class TestDataFactory {
 
     @Transactional
     public DonationReviewJpaEntity createDonationReview(String title, List<String> tags, String content, Long userId, Long donationId){
-        UserJpaEntity user = userRepository.findById(userId).get();
-        DonationJpaEntity donation = donationRepository.findById(donationId).get();
+        UserJpaEntity user = userRepository.findById(userId).orElseThrow(() -> new IllegalArgumentException(INVALID_PARAMETERS));
+        DonationJpaEntity donation = donationRepository.findById(donationId).orElseThrow(() -> new IllegalArgumentException(INVALID_PARAMETERS));
         String flattenTags = tags.stream().collect(Collectors.joining("#"));
         DonationReviewJpaEntity donationReview = new DonationReviewJpaEntity(null, title, "요약", flattenTags, content, "이미지", user, donation);
         return donationReviewRepository.save(donationReview);
+    }
+
+    public void createDonationRecord(Long userId, Long donationId, Long amount) {
+        if(amount < 0){
+            throw new IllegalArgumentException(INVALID_PARAMETERS);
+        }
+        UserJpaEntity userJpaEntity = userRepository.findById(userId).orElseThrow(() -> new IllegalArgumentException(INVALID_PARAMETERS));
+        DonationJpaEntity donationJpaEntity = donationRepository.findById(donationId).orElseThrow(() -> new IllegalArgumentException(INVALID_PARAMETERS));
+        System.out.println(donationJpaEntity.getEndDate());
+        DonationJpaEntity newDonationJpaEntity = new DonationJpaEntity(donationJpaEntity.getId(),
+                donationJpaEntity.getTitle(),
+                donationJpaEntity.isAnonymous(),
+                donationJpaEntity.getStartDate(),
+                donationJpaEntity.getEndDate(),
+                donationJpaEntity.getDescription(),
+                donationJpaEntity.getTargetAmount(),
+                donationJpaEntity.getCurrentAmount(),
+                donationJpaEntity.getViews(),
+                donationJpaEntity.getUserJpaEntity(),
+                donationJpaEntity.getPartnerJpaEntity(),
+                donationJpaEntity.getDonationItemJpaEntities(),
+                donationJpaEntity.getDonationImageJpaEntities(),
+                donationJpaEntity.getDonationCategories());
+        donationRepository.save(newDonationJpaEntity);
+        donationRecordRepository.save(new DonationRecordJpaEntity(null, amount, userJpaEntity, newDonationJpaEntity));
     }
 }
