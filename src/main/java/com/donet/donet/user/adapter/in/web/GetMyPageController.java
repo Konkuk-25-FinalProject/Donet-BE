4e7c49de40b5277e73c3f276b746a55ba4adc90b
JangIkhwan <bluensky0213@gmail.com>
2025-11-09 15:06:21 +0900

    [Docs] #54 마이페이지 조회 API 명세 작성
    - 마이페이지 조회 API에 명세 작성

--- src/main/java/com/donet/donet/user/adapter/in/web/GetMyPageController.java
+++ src/main/java/com/donet/donet/user/adapter/in/web/GetMyPageController.java
@@ -2,22 +2,34 @@ package com.donet.donet.user.adapter.in.web;
 
 import com.donet.donet.global.annotation.CurrentUserId;
 import com.donet.donet.global.response.BaseResponse;
+import com.donet.donet.global.swagger.CustomExceptionDescription;
 import com.donet.donet.user.adapter.in.web.dto.GetMyPageApiResponse;
 import com.donet.donet.user.application.port.in.GetMyPageUsecase;
 import com.donet.donet.user.application.port.in.dto.GetMyPageResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import static com.donet.donet.global.swagger.SwaggerResponseDescription.GET_MY_PAGE;
+
 @Slf4j
 @RequiredArgsConstructor
 @RestController
-public class GetMyPageController {
+public class GetMyPageController implements UserController{
     private final GetMyPageUsecase getMyPageUsecase;
 
+    @Operation(
+            summary = "마이페이지 조회 API",
+            description = """
+                    유저의 마이페이지를 조회할 수 있다. 토큰 인증이 필요하다
+                    """
+    )
+    @CustomExceptionDescription(GET_MY_PAGE)
     @GetMapping("users/me")
-    public BaseResponse<GetMyPageApiResponse> getMyPage(@CurrentUserId Long userId){
+    public BaseResponse<GetMyPageApiResponse> getMyPage(@Parameter(hidden = true) @CurrentUserId Long userId){
         log.info("[getMyPage] userId = {}", userId);
         GetMyPageResponse resp = getMyPageUsecase.getPage(userId);
         return new BaseResponse<>(GetMyPageApiResponse.from(resp));
