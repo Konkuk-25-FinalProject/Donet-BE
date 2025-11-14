8e7825545176e11ed1897c5b396f357aac776a8c
imscow11253 <seaweed8759@naver.com>
2025-11-14 18:19:52 +0900

    [Fix] #56 기부 금액 추가하는 로직을 application Service 클래스에서 하도록 수정

--- src/main/java/com/donet/donet/donation/application/AddDonationRecordService.java
+++ src/main/java/com/donet/donet/donation/application/AddDonationRecordService.java
@@ -3,7 +3,9 @@ package com.donet.donet.donation.application;
 import com.donet.donet.donation.application.port.in.AddDonationRecordUsecase;
 import com.donet.donet.donation.application.port.in.dto.command.AddDonationRecordCommand;
 import com.donet.donet.donation.application.port.out.CreateDonationRecordPort;
+import com.donet.donet.donation.application.port.out.FindDonationPort;
 import com.donet.donet.donation.application.port.out.UpdateDonationPort;
+import com.donet.donet.donation.domain.Donation;
 import com.donet.donet.donation.domain.DonationRecord;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -14,10 +16,13 @@ public class AddDonationRecordService implements AddDonationRecordUsecase {
 
     private final CreateDonationRecordPort createDonationRecordPort;
     private final UpdateDonationPort updateDonationPort;
+    private final FindDonationPort findDonationPort;
 
     @Override
     public void addDonationRecord(AddDonationRecordCommand command) {
-        updateDonationPort.addDonationAmount(command.donationId(), command.donationAmount());
+        Donation donation = findDonationPort.findDonationById(command.donationId());
+        donation.addAmount(command.donationAmount());
+        updateDonationPort.addDonationAmount(donation);
 
         DonationRecord donationRecord = DonationRecord.builder()
                 .userId(command.userId())
