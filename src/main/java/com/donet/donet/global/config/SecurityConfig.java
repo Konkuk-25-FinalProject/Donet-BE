86e0ab3a477c492aec83708904a34a56eb22fb5e
JangIkhwan <bluensky0213@gmail.com>
2025-11-17 14:53:05 +0900

    [Chore] #60 CORS 설정 추가
    - CORS 정책을 준수하기 위해서 프론트앱의 엔드포인트들을 허용하도록 설정

--- src/main/java/com/donet/donet/global/config/SecurityConfig.java
+++ src/main/java/com/donet/donet/global/config/SecurityConfig.java
@@ -10,6 +10,11 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
 
 @Configuration
 @EnableWebSecurity
@@ -31,6 +36,11 @@ public class SecurityConfig {
                 .sessionManagement(session->
                         session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)); // JWT 사용하니 세션을 사용하지 않음
 
+        http
+                .cors((cors) -> cors
+                        .configurationSource(corsConfigurationSource())
+                );
+
         // 엔드포인트별 인증 인가 설정
         http
                 .authorizeHttpRequests(request -> request
@@ -48,5 +58,17 @@ public class SecurityConfig {
                         configurer -> configurer.authenticationEntryPoint(customAuthenticationEntryPoint));
 
         return http.build();
+    }
+
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(List.of("http://localhost:3000", "http://127.0.0.1:3000", "https://donet-fe.vercel.app"));
+        configuration.setAllowedMethods(List.of("GET", "POST", "PATCH", "PUT", "DELETE", "OPTIONS"));
+        configuration.setAllowedHeaders(List.of("*")); // 모든 헤더 허용
+        configuration.setAllowCredentials(true); // 자격 증명 허용
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 }
