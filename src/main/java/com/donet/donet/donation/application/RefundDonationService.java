6470df423f506cdc3230442c72d3fc75da9d8293
imscow11253 <seaweed8759@naver.com>
2025-11-24 15:56:11 +0900

    [Refactor] #65 환불 비즈니스 로직에서 스마트 컨트랙트 결과값 로깅

--- src/main/java/com/donet/donet/donation/application/RefundDonationService.java
+++ src/main/java/com/donet/donet/donation/application/RefundDonationService.java
@@ -5,10 +5,12 @@ import com.donet.donet.donation.application.port.out.FindDonationPort;
 import com.donet.donet.donation.application.port.out.SmartContractPort;
 import com.donet.donet.donation.domain.Donation;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class RefundDonationService implements RefundDonationUsecase {
@@ -23,6 +25,18 @@ public class RefundDonationService implements RefundDonationUsecase {
                 .map(Donation::getId)
                 .toList();
 
-        smartContractPort.refundDonations(refundTargetDonationIds);
+        if (refundTargetDonationIds.isEmpty()) {
+            log.info("환불 대상 기부가 없습니다.");
+            return;
+        }
+
+        log.info("환불 처리 시작: {} 건", refundTargetDonationIds.size());
+        boolean success = smartContractPort.refundDonations(refundTargetDonationIds);
+
+        if (success) {
+            log.info("환불 처리 완료: {} 건", refundTargetDonationIds.size());
+        }else{
+            log.error("환불 처리 실패: donation IDs = {}", refundTargetDonationIds);
+        }
     }
 }
